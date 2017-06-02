5a33fdb5986cafe28be9834393439f243b135d30
Ronen Gilead-Raz <ronengi@gmail.com>
2017-06-03 00:30:35 +0300

    chapter.2.12.c
        Define DestroyShaders().

--- chapter.02/chapter.2.1.c
+++ chapter.02/chapter.2.1.c
@@ -289,3 +289,28 @@ void CreateShaders(void) {
     exit(-1);
     }
 }
+
+void DestroyShaders(void) {
+    GLenum ErrorCheckValue = glGetError();
+
+    glUseProgram(0);
+
+    glDetachShader(ProgramId, VertexShaderId);
+    glDetachShader(ProgramId, FragmentShaderId);
+
+    glDeleteShader(FragmentShaderId);
+    glDeleteShader(VertexShaderId);
+
+    glDeleteProgram(ProgramId);
+
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR) {
+    fprintf(
+        stderr,
+        "ERROR: Could not destroy the shaders: %s \n",
+        gluErrorString(ErrorCheckValue)
+    );
+
+    exit(-1);
+    }
+}
