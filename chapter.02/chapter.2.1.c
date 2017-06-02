e5f66b04d0206860637c514ce9db76262657462d
Ronen Gilead-Raz <ronengi@gmail.com>
2017-06-03 00:23:08 +0300

    chapter.2.9.c
        Add definition of CreateVBO().

--- chapter.02/chapter.2.1.c
+++ chapter.02/chapter.2.1.c
@@ -193,3 +193,45 @@ void Cleanup(void) {
     DestroyShaders();
     DestroyVBO();
 }
+
+void CreateVBO(void) {
+    GLfloat Vertices[] = {
+        -0.8f, -0.8f, 0.0f, 1.0f,
+         0.0f,  0.8f, 0.0f, 1.0f,
+         0.8f, -0.8f, 0.0f, 1.0f
+    };
+
+    GLfloat Colors[] = {
+        1.0f, 0.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, 1.0f
+    };
+
+    GLenum ErrorCheckValue = glGetError();
+
+    glGenVertexArrays(1, &VaoId);
+    glBindVertexArray(VaoId);
+
+    glGenBuffers(1, &VboId);
+    glBindBuffer(GL_ARRAY_BUFFER, VboId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &ColorBufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR) {
+        fprintf(
+            stderr,
+            "ERROR: Could not create a VBO: %s \n",
+            gluErrorString(ErrorCheckValue)
+        );
+
+    exit(-1);
+    }
+}
